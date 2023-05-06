7c73a686cc83952c3213240f93ff778732f68eb2
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-07 00:04:23 +0700

    feat(base_project): prepare project base
    to prepare base project make base, const, & using GetX state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            useMaterial3: false,
+            fontFamily: GoogleFonts.karla().fontFamily
+        ),
+        home: const Placeholder()
+    );
   }
 }
