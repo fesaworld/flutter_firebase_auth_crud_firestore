bf9ca7db1786031a0bfda24d2810aac7d488db69
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-07 00:41:31 +0700

    feat(splashscreen): add function to check user
    add add function to check user login. when user has login it will open the main page, if not then it will open the login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/splashscreen/splash_view.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screen/splashscreen/splash_controller.dart
+++ lib/screen/splashscreen/splash_controller.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase_auth_crud_firestore/screen/login/login_view.dart';
+import 'package:flutter_firebase_auth_crud_firestore/screen/mainpage/mainpage_view.dart';
 import 'package:get/get.dart';
 
 import '../../base/base_controller.dart';
 
 class SplashController extends BaseController {
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   @override
   void onInit() {
@@ -15,7 +19,12 @@ class SplashController extends BaseController {
 
   void timerStart() async {
     Timer(const Duration(seconds: 3), () async {
+
+      if(auth.currentUser != null){
+        Get.offAll(const MainpageView());
+      }else{
+        Get.offAll(const LoginView());
+      }
     });
   }
-
 }
