7d586bd1ecdd51a9e63e3dd7f228fb09a7a6d6f8
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-07 04:30:46 +0700

    feat(login_google): make function to login with google account
    make function to login with google account

--- lib/screen/login/login_controller.dart
+++ lib/screen/login/login_controller.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth_crud_firestore/base/base_controller.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/login/email/loginEmail_view.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/mainpage/mainpage_view.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginController extends BaseController {
 
@@ -19,12 +21,42 @@ class LoginController extends BaseController {
     Get.offAll(const LoginEmailView());
   }
 
-  void googleLogin() {
+  Future googleLogin() async {
+    GoogleSignInAccount? account = await GoogleSignIn().signIn();
+    if(account != null) {
+      try {
+        GoogleSignInAuthentication auth = await account.authentication;
+        OAuthCredential credential = GoogleAuthProvider.credential(
+            accessToken: auth.accessToken,
+            idToken: auth.idToken
+        );
 
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+        Get.offAll(const MainpageView());
+      } on FirebaseException catch (e) {
+        showSnackbar(
+            title: 'Error',
+            subTitle: e.message
+        );
+      }
+    }
   }
 
   void phoneLogin() {
 
+  }
+
+  void showSnackbar({required String title, required subTitle,}){
+    Get.snackbar(
+        title,
+        subTitle,
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        icon: const Icon(Icons.warning, color: Colors.white),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 5)
+    );
   }
 
 }
--- lib/screen/mainpage/mainpage_controller.dart
+++ lib/screen/mainpage/mainpage_controller.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase_auth_crud_firestore/base/base_controller.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/login/login_view.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class MainpageController extends BaseController{
 
@@ -15,6 +16,7 @@ class MainpageController extends BaseController{
 
   void logout() {
     try {
+      GoogleSignIn().signOut();
       FirebaseAuth.instance.signOut();
       Get.offAll(const LoginView());
     } on FirebaseException catch (e) {
