58965af9fdb39d9fba84d640a4e9157289fb6d93
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-08 22:09:09 +0700

    feat(note_dialog_widget): make widget note
    This widget will be called and display the data note in the dialog widget

---
+++ lib/widget/dialog/update_notes_dialog_widget.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth_crud_firestore/const/color_pallete.dart';
+import 'package:get/get.dart';
+
+import '../../const/text_style.dart';
+
+class UpdateDialogWidget extends StatelessWidget {
+  final String titleNote;
+  final String subTitleNote;
+  final VoidCallback? onDeleted;
+  final VoidCallback? onSave;
+  UpdateDialogWidget({Key? key, required this.titleNote, required this.subTitleNote, required this.onDeleted, required this.onSave}) : super(key: key);
+
+  TextEditingController noteTitleController = TextEditingController();
+  TextEditingController noteBodyController = TextEditingController();
+  FocusNode noteTitleFocus = FocusNode();
+  FocusNode noteBodyFocus = FocusNode();
+
+
+  @override
+  Widget build(BuildContext context) {
+    noteTitleController.text = titleNote;
+    noteBodyController.text = subTitleNote;
+
+
+    return GestureDetector(
+      onTap: (){
+        FocusManager.instance.primaryFocus?.unfocus();
+        noteTitleFocus.unfocus();
+        noteBodyFocus.unfocus();
+      },
+      child: AlertDialog(
+        backgroundColor: Colors.orange.shade50,
+        title: Container(
+            height: Get.height * 0.1,
+            child: TextField(
+              style: title,
+              focusNode: noteTitleFocus,
+              expands: true,
+              textAlign: TextAlign.start,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              controller: noteTitleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius.circular(8)),
+              ),
+            )
+        ),
+        content: Container(
+          height: Get.height * 0.3,
+          child: TextField(
+            style: subTitle,
+            focusNode: noteBodyFocus,
+            expands: true,
+            textAlign: TextAlign.start,
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            controller: noteBodyController,
+            decoration: const InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+            ),
+          )
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            ColorPalette.danger)),
+                    onPressed: () {
+                      Get.dialog(
+                        barrierDismissible: false,
+                           WarningDialogWidget(onDelete: onDeleted)
+                      );
+                    },
+                    child: const Text("Hapus")),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              SizedBox(
+                width: 150,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.orange.shade900)),
+                    onPressed: onSave,
+                    child: const Text("Simpan", style: TextStyle(color: ColorPalette.white),)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WarningDialogWidget extends StatelessWidget {
+  final VoidCallback? onDelete;
+  const WarningDialogWidget({Key? key, required this.onDelete}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Warning',style: title),
+      content: Text('Apakah yakin anda menghapus data ?', style: subTitle.copyWith(fontSize: 16)),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          ColorPalette.danger)),
+                  onPressed: onDelete,
+                  child: const Text("Hapus")),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Colors.orange.shade900)),
+                  onPressed: (){
+                    Get.back(result: false);
+                  },
+                  child: const Text("Batal")),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+
