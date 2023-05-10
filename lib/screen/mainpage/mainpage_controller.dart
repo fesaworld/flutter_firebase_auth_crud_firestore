77fd378ae207aafdb7015cf3482227628ae88638
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-10 15:59:57 +0700

    feat(read_data): make ui and function to read
    make ui and function to read data from firebase firestore using get, save respon to model

--- lib/screen/mainpage/mainpage_controller.dart
+++ lib/screen/mainpage/mainpage_controller.dart
@@ -1,17 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase_auth_crud_firestore/base/base_controller.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/login/login_view.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class MainpageController extends BaseController{
+import '../../const/text_style.dart';
+import '../../data/model/notes_model.dart';
+import '../../widget/dialog/update_notes_dialog_widget.dart';
+
+class MainpageController extends BaseController {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference? notesCollection;
+
+  List<Notes> noteList = [];
+  String? uuid;
+  bool isLoading = false;
 
   @override
-  void onInit() {
-  }
+  Future<void> onInit() async {
+    uuid = FirebaseAuth.instance.currentUser?.uid;
+    notesCollection = firestore.collection('notes');
 
-  void checkUser() {
-
+    readData(uuid: uuid!);
+    update();
   }
 
   void logout() {
@@ -24,4 +36,25 @@ class MainpageController extends BaseController{
     }
   }
 
+  Future readData({required String uuid}) async {
+    noteList = [];
+    isLoading = true;
+    update();
+
+    try{
+      QuerySnapshot<Map<String, dynamic>> snapshot = await firestore
+          .collection("notes")
+          .where('uuid', isEqualTo: uuid)
+          .orderBy('timestamp', descending: false)
+          .get();
+      noteList = snapshot.docs
+          .map((docSnapshot) => Notes.fromDocumentSnapshot(docSnapshot))
+          .toList();
+
+      isLoading = false;
+      update();
+    } on FirebaseException catch (e){
+      print(e.message);
+    }
+  }
 }
--- lib/screen/mainpage/mainpage_view.dart
+++ lib/screen/mainpage/mainpage_view.dart
@@ -1,14 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth_crud_firestore/const/text_style.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/mainpage/mainpage_controller.dart';
 import 'package:get/get.dart';
 
+import '../../widget/card/note_card_widget.dart';
+
 class MainpageView extends StatelessWidget {
   const MainpageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // FirebaseFirestore firestore = FirebaseFirestore.instance;
+    // CollectionReference notes = firestore.collection('notes');
+
     return GetBuilder<MainpageController>(
         init: MainpageController(),
         builder: (controller){
@@ -19,44 +25,119 @@ class MainpageView extends StatelessWidget {
                 style: title,
               ),
             ),
-            body: SafeArea(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                        'Welcome Mainpage',
-                        style: title
-                    ),
-                    const SizedBox(height: 10),
-                    StreamBuilder<User?>(
-                        stream: FirebaseAuth.instance.userChanges(),
-                        builder: (context, snapshot) {
-                          if(snapshot.hasData){
-                            return Text('Log In: ${snapshot.data?.uid}');
-                          }else{
-                            return const Text("Select one method to sign in");
-                          }
-                        }
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                      width: 150,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.all(Colors.lightBlue.shade900)),
-                          onPressed: () {
-                            controller.logout();
-                          },
-                          child: const Text("Log Out")),
-                    ),
-                  ],
-                )
+            body: RefreshIndicator(
+              onRefresh: () async {
+                controller.noteList = [];
+                controller.onInit();
+              },
+              child: SafeArea(
+                child: Container(
+                    height: Get.height,
+                    width: Get.width,
+                    child: SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+                      child: Column(children: [
+                        // FutureBuilder<QuerySnapshot>(
+                        //   future: notes.get(),
+                        //   builder: (_, snapshot){
+                        //     if(snapshot.hasData){
+                        //       return Column(
+                        //         children: snapshot.data!.docs.map((e) => NotesCardWidget(
+                        //           onTap: (){
+                        //
+                        //           },
+                        //           titleNote: (e.data() as dynamic)['title'].toString(),
+                        //           subTitleNote: (e.data() as dynamic)['body'].toString(),
+                        //         )).toList(),
+                        //       );
+                        //     }else{
+                        //       return Text('Loading');
+                        //     }
+                        //   }
+                        // ),
+
+                        // StreamBuilder<QuerySnapshot>(
+                        //     stream: notes.snapshots(),
+                        //     builder: (_, snapshot) {
+                        //       if (snapshot.hasData) {
+                        //         return Column(
+                        //           children: snapshot.data!.docs
+                        //               .map((e) => NotesCardWidget(
+                        //                     onTap: () {},
+                        //                     titleNote:
+                        //                         (e.data() as dynamic)['title']
+                        //                             .toString(),
+                        //                     subTitleNote:
+                        //                         (e.data() as dynamic)['body']
+                        //                             .toString(),
+                        //                   ))
+                        //               .toList(),
+                        //         );
+                        //       } else {
+                        //         return Text('Loading');
+                        //       }
+                        //     }),
+
+                        const SizedBox(height: 10),
+                        Text('Welcome Mainpage', style: title),
+                        const SizedBox(height: 10),
+                        StreamBuilder<User?>(
+                            stream: FirebaseAuth.instance.userChanges(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return Text('Log In: ${snapshot.data?.uid}');
+                              } else {
+                                return const Text("Select one method to sign in");
+                              }
+                            }),
+                        const SizedBox(height: 15),
+                        Text(controller.uuid ?? 'kosong'),
+                        const SizedBox(height: 15),
+                        SizedBox(
+                          width: 150,
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Colors.lightBlue.shade900)),
+                              onPressed: () {
+                                controller.logout();
+                              },
+                              child: const Text("Log Out")),
+                        ),
+
+                        controller.isLoading
+                            ? Center(
+                                child: Column(
+                                  children: const [
+                                    SizedBox(height: 20),
+                                    CircularProgressIndicator.adaptive(),
+                                    SizedBox(height: 10),
+                                    Text('Loading')
+                                  ],
+                                ),
+                              )
+                            :controller.noteList.length > 0
+                                  ? ListView.builder(
+                                      physics: NeverScrollableScrollPhysics(),
+                                      scrollDirection: Axis.vertical,
+                                      shrinkWrap: true,
+                                      itemCount: controller.noteList?.length,
+                                      itemBuilder: (context, index) {
+                                        return NotesCardWidget(
+                                            onTap: () {
+                                            },
+                                            titleNote: (controller.noteList as dynamic)[index].title,
+                                            subTitleNote: (controller.noteList as dynamic)[index].body,
+                                        );
+                                      })
+                                  :  const Center(
+                                      child: Text('Data not found'),
+                                    )
+                      ]),
+                    )),
               ),
             ),
           );
-        }
-    );
+        });
   }
 }
