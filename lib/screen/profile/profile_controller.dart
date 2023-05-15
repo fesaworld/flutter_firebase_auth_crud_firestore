fbc3ceac0a59e7a6dd8ae827b2b9b9b9f004bed5
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-15 22:44:56 +0700

    feat(profilscreen): add image, image picker and firebase storage
    add image, image picker and firebase storage function and ui

--- lib/screen/profile/profile_controller.dart
+++ lib/screen/profile/profile_controller.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_firebase_auth_crud_firestore/base/base_controller.dart';
 import 'package:flutter_firebase_auth_crud_firestore/data/secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 import '../login/login_view.dart';
 
@@ -13,11 +18,20 @@ class ProfileController extends BaseController{
   final Storage _storage = Storage();
   String? uuid;
 
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference? userCollection;
+
+  final ImagePicker picker = ImagePicker();
+  bool havePicture = false;
+  String? id;
+  String? imageLink;
+
   @override
   Future<void> onInit() async {
-    super.onInit();
-
     uuid = await _storage.readUid();
+    userCollection = firestore.collection('users');
+
+    update();
   }
 
   void logout() {
@@ -28,5 +42,47 @@ class ProfileController extends BaseController{
     } on FirebaseException catch (e) {
       print(e.message);
     }
+  }
+
+  Future uploadImage({required String uuid}) async {
+
+    XFile? filePicker = await picker.pickImage(source: ImageSource.gallery);
+
+    if(filePicker != null){
+      String fileName = basename(filePicker.path);
+
+      var ref = FirebaseStorage.instance.ref().child(fileName);
+      var task = ref.putFile(File(filePicker.path.toString()));
+      var snapshot = task.snapshot;
+      var link = await snapshot.ref.getDownloadURL();
+
+      imageLink = link.toString();
+      uploadPicture(
+          uuid: uuid,
+          profileImage: link.toString()
+      );
+    }
+
+    update();
+  }
+
+  Future uploadPicture(
+      {required String uuid,
+        required String profileImage}) async {
+
+    if(havePicture){
+      await userCollection?.doc(id).update({
+        'profileImage': profileImage,
+        'timestamp': DateTime.now()
+      });
+    }else{
+      await userCollection?.add({
+        'uuid': uuid,
+        'profileImage': profileImage,
+        'timestamp': DateTime.now()
+      });
+    }
+
+    // readData(uuid:uuid);
   }
 }
--- lib/screen/profile/profile_view.dart
+++ lib/screen/profile/profile_view.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth_crud_firestore/screen/profile/profile_controller.dart';
 import 'package:get/get.dart';
+import '../../const/color_pallete.dart';
 import '../../const/text_style.dart';
 
 class ProfileView extends StatelessWidget {
@@ -21,32 +23,103 @@ class ProfileView extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 10),
-                  Text('Welcome Mainpage', style: title),
-                  const SizedBox(height: 10),
-                  StreamBuilder<User?>(
-                      stream: FirebaseAuth.instance.userChanges(),
-                      builder: (context, snapshot) {
+                  // const SizedBox(height: 10),
+                  // Text('Welcome Mainpage', style: title),
+                  // const SizedBox(height: 10),
+                  // StreamBuilder<User?>(
+                  //     stream: FirebaseAuth.instance.userChanges(),
+                  //     builder: (context, snapshot) {
+                  //       if (snapshot.hasData) {
+                  //         return Text('Log In: ${snapshot.data?.uid}');
+                  //       } else {
+                  //         return const Text("Select one method to sign in");
+                  //       }
+                  //     }),
+                  // const SizedBox(height: 15),
+                  // Text(controller.uuid ?? 'kosong'),
+                  // const SizedBox(height: 15),
+                  // SizedBox(
+                  //   width: 150,
+                  //   child: ElevatedButton(
+                  //       style: ButtonStyle(
+                  //           backgroundColor: MaterialStateProperty.all(
+                  //               Colors.lightBlue.shade900)),
+                  //       onPressed: () {
+                  //         controller.logout();
+                  //       },
+                  //       child: const Text("Log Out")),
+                  // ),
+
+                  StreamBuilder<QuerySnapshot>(
+                      stream: controller.userCollection
+                          ?.where('uuid', isEqualTo: controller.uuid)
+                          .snapshots(),
+                      builder: (_, snapshot) {
                         if (snapshot.hasData) {
-                          return Text('Log In: ${snapshot.data?.uid}');
-                        } else {
-                          return const Text("Select one method to sign in");
+                          return Column(
+                            children: [
+                              Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border:
+                                    Border.all(color: ColorPalette.primary),
+                                    image: DecorationImage(
+                                        image: NetworkImage(snapshot.data?.docs.length != 0 ? snapshot.data?.docs[0]['profileImage'] : 'https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black-thumbnail.png'),
+                                        fit: BoxFit.cover)),
+                              ),
+                              const SizedBox(height: 10),
+                              SizedBox(
+                                width: 150,
+                                child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.all(
+                                            Colors.green)),
+                                    onPressed: () async {
+                                      await controller.uploadImage(
+                                          uuid: controller.uuid!);
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(Icons.upload),
+                                        Text(snapshot.data?.docs.length != 0 ? 'Update' : "Upload"),
+                                      ],
+                                    )),
+                              ),
+                              const SizedBox(height: 10),
+                              Text('Welcome', style: title),
+                              const SizedBox(height: 10),
+                              Text(
+                                  'Log In: ${snapshot.data?.docs.length != 0 ? snapshot.data?.docs[0]['uuid'] : 'guest'}'),
+                              const SizedBox(height: 15),
+                              SizedBox(
+                                width: 150,
+                                child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.all(
+                                            Colors.lightBlue.shade900)),
+                                    onPressed: () {
+                                      controller.logout();
+                                    },
+                                    child: const Text("Log Out")),
+                              ),
+                            ],
+                          );
+                        }else {
+                          return Column(
+                            children: [
+                              CircularProgressIndicator.adaptive(),
+                              SizedBox(height: 10,),
+                              Text('Loading'),
+                            ],
+                          );
                         }
                       }),
-                  const SizedBox(height: 15),
-                  Text(controller.uuid ?? 'kosong'),
-                  const SizedBox(height: 15),
-                  SizedBox(
-                    width: 150,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Colors.lightBlue.shade900)),
-                        onPressed: () {
-                          controller.logout();
-                        },
-                        child: const Text("Log Out")),
-                  ),
                 ],
               ),
             ),
